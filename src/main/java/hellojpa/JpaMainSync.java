c7d8dec74d13bae50e08145f4e689fc8f3044ddd
gunners <chakagae123@naver.com>
2024-04-09 01:36:35 +0900

    * 섹션 8. 프록시 2

--- src/main/java/hellojpa/JpaMainSync.java
+++ src/main/java/hellojpa/JpaMainSync.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
+import jakarta.persistence.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -82,19 +80,20 @@ public class JpaMainSync {
 //            em.flush();
 //            em.clear();
 
-            MemberSync member = new MemberSync();
-            member.setUsername("hello");
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-//            MemberSync findMember = em.find(MemberSync.class, member.getId());
-            MemberSync findMember = em.getReference(MemberSync.class, member.getId()); //프록시객체 -> 쿼리가 나가지않는다
-            System.out.println("findMember = " + findMember.getClass()); //프록시클래스임을 확인(쿼리 나가지않는다.)
-            System.out.println("findMember.getId() = " + findMember.getId()); //위의 코드에서 파라미터로 썼기때문에 쿼리 나가지않는다
-            System.out.println("findMember.getUsername() = " + findMember.getUsername()); //실제 findMember를 쓸 때 쿼리가 나간다.
+//            MemberSync member = new MemberSync();
+//            member.setUsername("hello");
+//
+//            em.persist(member);
+//
+//            em.flush();
+//            em.clear();
+//
+////            MemberSync findMember = em.find(MemberSync.class, member.getId());
+//            MemberSync findMember = em.getReference(MemberSync.class, member.getId()); //프록시객체 -> 쿼리가 나가지않는다
+//            System.out.println("before findMember = " + findMember.getClass()); //프록시클래스임을 확인(쿼리 나가지않는다.)
+//            System.out.println("findMember.getId() = " + findMember.getId()); //위의 코드에서 파라미터로 썼기때문에 쿼리 나가지않는다
+//            System.out.println("findMember.getUsername() = " + findMember.getUsername()); //실제 findMember를 쓸 때 쿼리가 나간다.
+//            System.out.println("after findMember = " + findMember.getClass()); //프록시객체는 실제 엔티티로 바뀌는것이 아님을 확인할 수 있다.
 
 
 //            printMember(findMember);
@@ -102,9 +101,104 @@ public class JpaMainSync {
 //            printMemberAndTeam(member);
 
 
+
+
+//            MemberSync member1 = new MemberSync();
+//            member1.setUsername("member1");
+//            em.persist(member1);
+//
+//            MemberSync member2 = new MemberSync();
+//            member2.setUsername("member2");
+//            em.persist(member2);
+//
+//            em.flush();
+//            em.clear();
+//
+//            System.out.println("1");
+//            MemberSync m1 = em.find(MemberSync.class, member1.getId());
+//            System.out.println("2");
+//            MemberSync m2 = em.getReference(MemberSync.class, member2.getId());
+//
+//            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass())); //같은 class의 선언이더라도 프록시 객체는 원본 클래스와 같지않다
+//                                                                                  //JPA에서 class 비교는 == 으로 하지 않는게 좋다
+//            System.out.println("m1 instance m2 : " + (m1 instanceof MemberSync));
+//            System.out.println("m1 instance m2 : " + (m2 instanceof MemberSync));
+
+
+
+//            MemberSync member1 = new MemberSync();
+//            member1.setUsername("member1");
+//            em.persist(member1);
+//
+//            em.flush();
+//            em.clear();
+//
+//            MemberSync m1 = em.find(MemberSync.class, member1.getId());
+//            System.out.println("m1 = " + m1.getClass());
+//
+//            MemberSync reference = em.getReference(MemberSync.class, member1.getId());
+//            System.out.println("reference = " + reference.getClass());
+//
+//            System.out.println("a == a : " + (m1 == reference));
+
+
+
+
+//            MemberSync member1 = new MemberSync();
+//            member1.setUsername("member1");
+//            em.persist(member1);
+//
+//            em.flush();
+//            em.clear();
+//
+//            MemberSync refMember = em.getReference(MemberSync.class, member1.getId());
+//            System.out.println("refMember = " + refMember.getClass()); //proxy
+//
+//            MemberSync findMember = em.find(MemberSync.class, member1.getId());
+//            System.out.println("findMember = " + findMember.getClass()); //MemberSync
+//
+//            System.out.println("refMember == findMember : " + (refMember == findMember)); //true가 나와야하므로 em.find에서도 프록시객체를 반환함
+
+
+
+
+//            MemberSync member1 = new MemberSync();
+//            member1.setUsername("member1");
+//            em.persist(member1);
+//
+//            em.flush();
+//            em.clear();
+//
+//            MemberSync refMember = em.getReference(MemberSync.class, member1.getId());
+//            System.out.println("refMember = " + refMember.getClass()); //proxy
+//
+//            em.detach(refMember);
+//
+//            refMember.getUsername(); //영속성 컨텍스트에 해당 객체가 없으므로 프록시객체 초기화가 불가능
+
+
+
+
+            MemberSync member1 = new MemberSync();
+            member1.setUsername("member1");
+            em.persist(member1);
+
+            em.flush();
+            em.clear();
+
+            MemberSync refMember = em.getReference(MemberSync.class, member1.getId());
+            System.out.println("refMember = " + refMember.getClass()); //proxy
+            Hibernate.initialize(refMember);// 강제 초기화 : refMember.getUsername(); 같은 경우도 강제초기화(강제호출)이다.
+
+            System.out.println("isLoaded : " + emf.getPersistenceUnitUtil().isLoaded(refMember)); //프록시 인스턴스의 초기와 여부 확인
+
+
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
