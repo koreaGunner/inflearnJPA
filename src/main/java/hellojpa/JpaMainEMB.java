5e89ec902bacac8c14e9c28e693f9a84ae0d0de6
gunners <chakagae123@naver.com>
2024-04-11 15:29:08 +0900

    * 섹션 9. 기본값 타입, 임베디드 타입

---
+++ src/main/java/hellojpa/JpaMainEMB.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMainEMB {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        //엔티티 매니저는 쓰레드간에 공유X(사용하고 버려야된다)
+        EntityManager em = emf.createEntityManager();
+
+        //!! JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+
+        //jpa는 트랜잭션이 매우 중요함
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            MemberEMB member = new MemberEMB();
+            member.setUsername("hello");
+            member.setHomeAddress(new Address("city", "street", "zipcode"));
+            member.setWorkPeriod(new Period());
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
