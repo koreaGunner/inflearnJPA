a8f27c5fd99e072ab555c8698c239397a02c949c
gunners <chakagae123@naver.com>
2024-04-12 01:14:12 +0900

    * 섹션 9. 값 타입 컬렉션

--- src/main/java/hellojpa/MemberEMB.java
+++ src/main/java/hellojpa/MemberEMB.java
@@ -3,6 +3,10 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class MemberEMB {
@@ -30,6 +34,8 @@ public class MemberEMB {
     @Embedded
     private Address homeAddress;
 
+
+    //같은 객체를 Embedded 할 때
 //    @Embedded
 //    @AttributeOverrides({
 //            @AttributeOverride(name = "city",
@@ -40,6 +46,26 @@ public class MemberEMB {
 //            column = @Column(name = "WORK_ZIPCODE"))
 //    })
 //    private Address workAddress;
+
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    //실무에서는 사용하지않는다.!
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns =
+//        @JoinColumn(name = "MEMBER_ID")
+//    )
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    //값타입 컬렉션의 대체자는 일대다 관계를 고려하는 것이 차라리 낫다.
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -71,5 +97,29 @@ public class MemberEMB {
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+//    public List<Address> getAddressHistory() {
+//        return addressHistory;
+//    }
+//
+//    public void setAddressHistory(List<Address> addressHistory) {
+//        this.addressHistory = addressHistory;
+//    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
